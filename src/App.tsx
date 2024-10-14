ef130c0099e107309f48a9b38f961e18957d8f42
Davi S. Nascimento <nascimentoos.davi@gmail.com>
2024-10-14 16:31:11 -0300

    v1.0.0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Login from './screen/login'
+// import Login from './screen/login'
 import Main from './screen/main'
 
 function App() {
 
   return (
     <>
-      <Login/>
+      <Main/>
     </>
   )
 }
--- src/screen/main/index.tsx
+++ src/screen/main/index.tsx
@@ -1,10 +1,22 @@
-import Login from "../login";
-import { Container } from "./styled";
-
+import { Container, Header, Icon, Logo, Sidebar, SidebarBlock } from "./styled";
+import { useState } from 'react';
 export default function Main () {
+    const [user] = useState('Davi');
     return (
         <Container>
-            
+            <Sidebar>
+                <SidebarBlock>
+                    <Logo src="../../assets/Logo_Hanna_2.png" alt="Logo"/>
+                    <Icon>☰</Icon>
+                    <Icon>🔔</Icon>
+                    <Icon>⚙️</Icon>
+                    <Icon>👤</Icon>
+                </SidebarBlock>
+            </Sidebar>
+            <Header>
+                <h1>Olá {user}!</h1>
+            </Header>
+
         </Container>
     )
 }

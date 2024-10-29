0b04b282dfaf10eb31cd6a7434ca8c6959b4f3b9
Davi S. Nascimento <nascimentoos.davi@gmail.com>
2024-10-29 19:56:21 -0300

    integração Voz AI no projeto

--- src/components/AI/index.tsx
+++ src/components/AI/index.tsx
@@ -1,8 +1,65 @@
 import { TiMicrophoneOutline } from "react-icons/ti";
 import { Comand, ContainerComand, ContainerHeader, ContainerInput, ContainerTitle, IMG, Searchbox, TextComando, Title } from "./styled";
 import { PiLightbulbLight, PiLightningFill } from "react-icons/pi";
+import React, { useState, useEffect } from 'react';
+
+
+
+
 
 export default function AssistenteAI() {
+
+    const VoiceRecognition: React.FC = () =>{
+
+        const [transcript, setTranscript] = useState<string>('');
+        const [isListening, setIsListening] = useState<boolean>(false);
+        let recognition: SpeechRecognition | null = null;
+    
+        useEffect(() => {
+            if ('webkitSpeechRecognition' in window) {
+                recognition = new window.webkitSpeechRecognition();
+                recognition.lang = 'pt-BR';
+                recognition.continuous = true;
+                recognition.interimResults = false;
+    
+    
+                recognition.onresult = (event: SpeechRecognitionEvent) => {
+                    const currentTranscript = event.results[event.results.length - 1][0].transcript;
+                    setTranscript('')
+                    setTranscript((prev) => prev + ' ' + currentTranscript);
+                  };
+    
+                  recognition.onstart = () => {
+                    console.log('Reconhecimento de voz iniciado');
+                  };
+    
+                  recognition.onend = () => {
+                    console.log('Reconhecimento de voz encerrado');
+                    setIsListening(false);
+                  };
+            } else {
+                console.log('esse navegador não suporta web Speech API');
+            }
+            return () => {
+                if (recognition) {
+                    recognition.stop();
+                }
+            };
+        }, []);
+        // Função para iniciar/parar o reconhecimento
+      const toggleListening = () => {
+        if (recognition) {
+          if (isListening) {
+            recognition.stop();
+            setIsListening(false);
+          } else {
+            recognition.start();
+            setIsListening(true);
+          }
+        }
+      };
+    };
+
     return (
         <>
             <ContainerHeader>
